571366ba943660cfab2eaa1a01ef1913ac6aede1
Malak Sabry <malaksabry367@gmail.com>
2025-08-01 03:07:26 +0300

    setting up the environmet and implement the navbar and make it functional and make its responsiveness

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,18 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-
+import { BrowserRouter as Router } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { AuthProvider } from "./context/AuthContext.tsx";
+const client = new QueryClient();
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={client}>
+      <AuthProvider>
+        <Router>
+          <App />
+        </Router>
+      </AuthProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
